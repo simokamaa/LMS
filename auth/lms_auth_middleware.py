5170ee4ec67fef93fccb7de33f9f56dd4bfe1bf1
Simon Kamau <simonkamau690@gmail.com>
2024-04-17 14:16:05 +0300

    succesful multi roles implementation

--- auth/lms_auth_middleware.py
+++ auth/lms_auth_middleware.py
@@ -1,8 +1,17 @@
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-class StudentMiddleware:
-    def __process_view(self, request, view_func, view_args, view_kwargs):
-        # Check if user is logged in and is a student
-        if request.user.is_authenticated and request.user.is_staff:  # Replace 'is_staff' with your student check logic
-            return redirect('index')  # Replace 'index' with your desired redirect URL
-        return None
+class StudentRedirectMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        # Check if the user is authenticated and in the "student" group
+        if request.user.is_authenticated and request.user.groups.filter(name='Student').exists():
+            # Redirect to the student dashboard
+            if not request.path.startswith(reverse('dashboard-student')):
+                return HttpResponseRedirect(reverse('dashboard-student'))
+
+        return response
--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "auth.lms_auth_middleware.StudentRedirectMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
