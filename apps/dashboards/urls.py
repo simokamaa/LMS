603e3efd15fb2745f70e584d049e81df17719560
Simon Kamau <simonkamau690@gmail.com>
2024-04-17 12:37:01 +0300

    implemented diffrent roles user dashboard

--- apps/dashboards/urls.py
+++ apps/dashboards/urls.py
@@ -15,4 +15,27 @@ urlpatterns = [
         login_required(DashboardsView.as_view(template_name="dashboard_crm.html")),
         name="dashboard-crm",
     ),
+    
+    path(
+        "dashboard/student",
+        login_required(DashboardsView.as_view(template_name="dashboard_analytics.html")),
+        name="dashboard-student",
+    ),
+
+    path( 
+        "dashboard/instructor/",
+        login_required(DashboardsView.as_view(template_name="dashboard_crm.html")),
+        name="dashboard-instructor",
+    ),
+    
+        path( 
+        "dashboard/general/",
+        login_required(DashboardsView.as_view(template_name="dashboard_general.html")),
+        name="dashboard-general",
+    ),
+    path( 
+        "dashboard/role/",
+        login_required(DashboardsView.as_view(template_name="new_role_dashboard.html")),
+        name="new_role_dashboard",
+    ),
 ]
