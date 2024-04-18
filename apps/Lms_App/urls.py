eccca82788e366158047a27692026a7fdc0d5a17
Simon Kamau <simonkamau690@gmail.com>
2024-04-18 18:37:52 +0300

    urls config

--- apps/Lms_App/urls.py
+++ apps/Lms_App/urls.py
@@ -3,5 +3,6 @@ from . views import LmsAppView
 
 
 urlpatterns = [
-    path("", LmsAppView.as_view(template_name=""), name="lms_app_index"),
+    path("", LmsAppView.as_view(template_name="lms_modules_management.html"), name="lms_module"),
+    path("app/new", LmsAppView.as_view(template_name="modal_create_app.html"), name="lms_app_index"),
 ]
