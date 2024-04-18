151ec950dae23bf395fa9a3f6fb1f911b806f08b
Simon Kamau <simonkamau690@gmail.com>
2024-04-18 14:51:53 +0300

    modules models

--- apps/Lms_App/models.py
+++ apps/Lms_App/models.py
@@ -1,3 +1,20 @@
+# models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    # Add other fields as needed
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    items = models.ManyToManyField(MenuItem)
+
+class App(models.Model):
+    name = models.CharField(max_length=100)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, related_name='apps')
+    menu = models.ForeignKey(Menu, on_delete=models.SET_NULL, null=True)
+
+
