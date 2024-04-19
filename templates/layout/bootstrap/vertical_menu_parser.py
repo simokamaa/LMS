c1acf1f807005ba91f300aee0213b8b0a1c26f22
Simon Kamau <simonkamau690@gmail.com>
2024-04-19 12:56:04 +0300

    paser modification

--- templates/layout/bootstrap/vertical_menu_parser.py
+++ templates/layout/bootstrap/vertical_menu_parser.py
@@ -1,13 +1,17 @@
 # views.py
 import json
+
 from django.conf import settings
-from .models import Menu, MenuItem
-menu_file_path =  settings.BASE_DIR / "templates" / "layout" / "partials" / "menu" / "vertical" / "json" / "vertical_menu.json"
 from django.http import HttpResponse
+
+from .models import Menu, MenuItem
+
+menu_file_path =  settings.BASE_DIR / "templates" / "layout" / "partials" / "menu" / "vertical" / "json" / "vertical_menu.json"
+
 
 def parse_vertical_menu(request):
     # Assuming vertical_menu.json is located in a specific directory
-    menu_file_path = '/path/to/vertical_menu.json'
+    menu_file_path = './vertical_menu.json'
 
     # Open and read the JSON file
     with open(menu_file_path, 'r') as f:
