d58fa92628915c61929fb6c6624a61c9b57d7d3a
Simon Kamau <simonkamau690@gmail.com>
2024-05-07 14:15:26 +0300

    badge count

--- apps/Lms_App/models.py
+++ apps/Lms_App/models.py
@@ -7,7 +7,7 @@ class MenuItem(models.Model):
     icon = models.CharField(max_length=100, blank=True, null=True)
     slug = models.CharField(max_length=100, blank=True, null=True)
     badge_color = models.CharField(max_length=20, null=True, blank=True)
-    badge = models.IntegerField(null=True, blank=True)
+    badge_count = models.IntegerField(null=True, blank=True)
     url = models.CharField(max_length=100, null=True, blank=True)
     external = models.BooleanField(default=False)
     permission = models.CharField(max_length=100, null=True, blank=True)
