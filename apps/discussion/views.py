b16c7edb89227e58ceb3219d4668055f4d6624e4
Simon Kamau <simonkamau690@gmail.com>
2024-05-20 13:59:45 +0300

    views module

--- apps/discussion/views.py
+++ apps/discussion/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from web_project import TemplateLayout
 
-# Create your views here.
+
+
+
+
+class discussionView(TemplateView):
+    # Predefined function
+    def get_context_data(self, **kwargs):
+        # A function to init the global layout. It is defined in web_project/__init__.py file
+        context = TemplateLayout.init(self, super().get_context_data(**kwargs))
+
+        return context
