e1896e6bad310f4ea6a38a9d1d481fb21ac7c69b
Simon Kamau <simonkamau690@gmail.com>
2024-05-20 12:13:39 +0300

    drf modules

---
+++ drf_apps/discusion_drf/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DiscusionDrfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'discusion_drf'
---
+++ drf_apps/grade_tools_drf/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GradeToolsDrfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'grade_tools_drf'
